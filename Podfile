7a10865ed8bb9fc285eeb9eb619c6ba7e2ce729b
Taka200 <tncyberport@gmail.com>
2019-02-10 06:59:32 +0800

    pod installと.gitignoreの作成まで実施

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '12.1'
+
+target 'taskapp' do
+
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for taskapp
+
+    pod 'RealmSwift', '3.11.1'
+
+end
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -1,21 +1,46 @@
-//
-//  ViewController.swift
-//  taskapp
-//
-//  Created by 中川貴裕 on 2019/02/09.
-//  Copyright © 2019 Taka200. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet weak var tableVIew: UITableView!
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    // MARK: UITableViewのDataSourceプロトコルのメソッド
+    // データの数を返すメソッド
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
 
+    // 各セルの内容を返すメソッド
+    func tableVIew(_ tableView: UITableView, cellForRowAt IndexPath: IndexPath) -> UITableViewCell {
+        //再利用可能なセルを得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: <#T##IndexPath#>)
+        
+        return cell
+    }
+    
+    // MARK: UITableViewDelegateプロトコルのメソッド
+    // 各セルを選択した時に実行されるメソッド
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+    }
+    
+    //セルが削除可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    // Deleteボタンが押された時に呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    }
+    
+    
 }
 
